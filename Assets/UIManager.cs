0512b599936b358cab112bebcd720ec47e6953b6
Svyatoslav <svyasem@gmail.com>
2019-10-05 16:36:06 +0300

    UIManager

---
+++ Assets/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+namespace Nothing
+{
+    public class UIManager : MonoBehaviour
+    {
+        public Player player;
+
+        public TextMeshProUGUI moneyText;
+
+        private void Update()
+        {
+            moneyText.text = "Money: " + player.Money;
+        }
+    }
+}
