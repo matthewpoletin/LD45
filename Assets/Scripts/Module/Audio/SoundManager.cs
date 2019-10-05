d45c5f23e56545f331c87d2bb9ddd732ee952c4b
Artemiy Seyfullaev <matterox96@gmail.com>
2019-10-05 21:33:51 +0300

    - Added sound manager

---
+++ Assets/Scripts/Module/Audio/SoundManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    private AudioSource musicSource;
+    private AudioClip currentClip;
+    private AudioClip nextClip;
+
+    private bool changeClip;
+
+    private void Awake() {
+        musicSource = GetComponent<AudioSource>();
+    }
+    
+     public void PlayMusic(AudioClip newClip) {
+         if (currentClip == null) {
+            ChangeClip(newClip);
+         } else {
+            nextClip = newClip;
+            changeClip = true;
+         }
+    }
+
+    private void Update() {
+        if(changeClip && musicSource.time >= currentClip.length) {
+            changeClip = false;
+            ChangeClip(nextClip);
+        }
+
+        if (musicSource.clip != null && !musicSource.isPlaying) {
+            musicSource.Play();
+        }
+    }
+
+    private void ChangeClip(AudioClip clip) {
+        currentClip = clip;
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+}
