c08df42ff165ddaa0abeee17ffbfe39755b3d9d4
matthewpoletin <poletinm@yandex.ru>
2019-10-05 18:15:34 +0300

    Базовая логика движения уровня

--- Assets/Scripts/Module/Game/Level/LevelManager.cs
+++ Assets/Scripts/Module/Game/Level/LevelManager.cs
@@ -6,11 +6,29 @@ using UnityEngine;
 
 namespace Module.Game.Level
 {
+    public class ChunkData
+    {
+        public ChunkData(GameObject gameObject, float chunkLength)
+        {
+            _gameObject = gameObject;
+            _chunkLength = chunkLength;
+        }
+
+        private GameObject _gameObject;
+        private float _chunkLength;
+
+        public GameObject GameObject => _gameObject;
+        public float StartPositionZ => _gameObject.transform.position.z;
+        public float EndPositionZ => _gameObject.transform.position.z + _chunkLength;
+    }
+
     public class LevelManager : MonoBehaviour
     {
         private const float CHUNK_LENGTH = 20.0f;
 
-        public float CurrentLevelMovemnetSpeed = 0f;  
+        private float _currentLevelMovementSpeed = 0.5f;
+
+        public float CurrentLevelMovementSpeed => Mathf.Max(0f, _currentLevelMovementSpeed);
 
         [SerializeField] private Transform chunksContainer = null;
 
@@ -23,6 +41,8 @@ namespace Module.Game.Level
 
         private List<GameObject> _chunkPrefabs = new List<GameObject>();
 
+        private List<ChunkData> _chunks = new List<ChunkData>();
+
         public void Init()
         {
             // Load prefab resources
@@ -33,16 +53,26 @@ namespace Module.Game.Level
             }
         }
 
-
         /// <summary>Create first level chunks</summary>
         public void CreateLevel()
         {
-            for (int i = 0; i < 10; i++)
+            for (var i = 0; i < 10; i++)
             {
-                var randomChunkPrefab = GetRandomChunk();
-                var chunkGameObject = GameModule.Instance.GameObjectPool.GetObject(randomChunkPrefab, chunksContainer);
-                chunkGameObject.transform.position = new Vector3(0, 0, i * CHUNK_LENGTH);
+                CreateChunk();
             }
+        }
+
+        private void CreateChunk()
+        {
+            var previousChunkZ =
+                _chunks.Count != 0 ? _chunks[_chunks.Count - 1].EndPositionZ : chunksContainer.transform.position.z;
+
+            var randomChunkPrefab = GetRandomChunk();
+            var chunkGameObject = GameModule.Instance.GameObjectPool.GetObject(randomChunkPrefab, chunksContainer);
+            chunkGameObject.transform.position = new Vector3(0, 0, previousChunkZ);
+
+            var chunkData = new ChunkData(chunkGameObject, CHUNK_LENGTH);
+            _chunks.Add(chunkData);
         }
 
         private GameObject GetRandomChunk()
@@ -50,6 +80,32 @@ namespace Module.Game.Level
             var index = Random.Range(0, _chunkPrefabs.Count);
             var randomChunkPrefab = _chunkPrefabs.ElementAt(index);
             return randomChunkPrefab;
+        }
+
+        public void Update()
+        {
+            // move all chunks
+            var movementDelta = -1f * CurrentLevelMovementSpeed * chunksContainer.transform.forward;
+            foreach (var chunkData in _chunks)
+            {
+                chunkData.GameObject.transform.position += movementDelta;
+            }
+
+            // remove old chunks
+            var firstChunk = _chunks[0];
+            var firstChunkStartPositionZ = firstChunk.StartPositionZ;
+            if (firstChunkStartPositionZ < chunksContainer.position.z - 100f)
+            {
+                _chunks.RemoveAt(0);
+                GameModule.Instance.GameObjectPool.UtilizeObject(firstChunk.GameObject);
+            }
+
+            // add more chunks
+            var lastChunkEndPositionZ = _chunks[_chunks.Count - 1].EndPositionZ;
+            if (lastChunkEndPositionZ < chunksContainer.position.z + 100f)
+            {
+                CreateChunk();
+            }
         }
     }
 }
