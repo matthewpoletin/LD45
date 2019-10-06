433386b21b58f9af6139fbf294b3b4f5eea172be
novikovantonbox <novikovantonbox@gmail.com>
2019-10-06 12:07:40 +0300

    add backgriund

---
+++ Assets/Prefabs/Background/BG_tube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_tube : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.transform.eulerAngles = new Vector3(0, Time.deltaTime, 0);
+        gameObject.transform.Rotate(0, Time.deltaTime * -2, 0);
+        
+    }
+}
