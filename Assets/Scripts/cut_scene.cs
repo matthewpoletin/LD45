c9015275a1816e460d5735ba4ab6930cb2bcff9e
venikamen <venikamen42@gmail.com>
2019-10-07 02:32:23 +0300

    Added cut-scene
    Сделана кат-сцена

---
+++ Assets/Scripts/cut_scene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cut_scene : MonoBehaviour
+{
+
+   public void StartGame()
+    {
+        Application.LoadLevel("Game");
+    }
+
+   
+}
