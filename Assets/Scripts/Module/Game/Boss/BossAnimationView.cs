cb3dbff673a29e789ddab0dde25f3bae99f91712
Artemiy Seyfullaev <matterox96@gmail.com>
2019-10-06 16:12:58 +0300

    boss view

---
+++ Assets/Scripts/Module/Game/Boss/BossAnimationView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Module.Game.Boss
+{
+    public class BossAnimationView : MonoBehaviour
+    {
+        [SerializeField] private Animator bossAnimator;
+
+        private void Start()
+        {
+
+        }
+
+        public void ShowBoss()
+        {
+            // bossAnimator.
+        }
+
+        public void AttackLeft()
+        {
+
+        }
+
+        public void AttackRight()
+        {
+
+        }
+    }
+}
