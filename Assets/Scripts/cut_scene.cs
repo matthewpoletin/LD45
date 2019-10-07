5cdb253c66b747ed4d4f1c75b0b92229b6afcfe0
venikamen <venikamen42@gmail.com>
2019-10-07 15:33:51 +0300

    cut-scene changes

--- Assets/Scripts/cut_scene.cs
+++ Assets/Scripts/cut_scene.cs
@@ -1,11 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 public class cut_scene : MonoBehaviour
 {
+    public PlayableDirector playableDirector;
+    public GameObject text;
 
-   public void StartGame()
+    public void TimeLine()
+    {
+            playableDirector.Pause();
+            text.SetActive(true);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey("f"))
+        {
+            playableDirector.Play();
+            text.SetActive(false);
+        }
+    }
+
+
+    public void StartGame()
     {
         Application.LoadLevel("Game");
     }
